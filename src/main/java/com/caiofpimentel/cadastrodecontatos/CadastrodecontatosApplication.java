b0deec8184a3549bd00a97656d2b5e6456845859
Caio <caio-pimentel@hotmail.com>
2025-05-28 15:53:50 -0300

    Initial commit

---
+++ src/main/java/com/caiofpimentel/cadastrodecontatos/CadastrodecontatosApplication.java
@@ -0,0 +1,13 @@
+package com.caiofpimentel.cadastrodecontatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrodecontatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastrodecontatosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caiofpimentel/cadastrodecontatos/CadastrodecontatosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caiofpimentel.cadastrodecontatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastrodecontatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
