c9e7b53fe0d8c697e0e4059696b24af96bfd41ce
Caio <caio-pimentel@hotmail.com>
2025-06-02 19:58:49 -0300

    Adiciona atributo ID para identificar os contatos

--- src/main/java/com/caiofpimentel/cadastrodecontatos/model/Contato.java
+++ src/main/java/com/caiofpimentel/cadastrodecontatos/model/Contato.java
@@ -1,6 +1,10 @@
 package com.caiofpimentel.cadastrodecontatos.model;
 
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -17,6 +21,11 @@ import lombok.Setter;
 public class Contato {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+
+    @Column(unique = true)
     private String telefone;
     
     private String nome;
